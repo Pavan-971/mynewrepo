0842643620a960ea4c00808a3ebb84cfb0ba3100
pavan <you@example.com>
2020-12-21 05:50:03 +0000

    update

--- main.tf
+++ main.tf
@@ -14,3 +14,17 @@ key_name = "mykeypair"
 
 
 }
+
+
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "infypavan"
+    key            = "global/s3/terraform.tfstate"
+    region         = "ap-south-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "pavantable"
+    encrypt        = true
+  }
+}
